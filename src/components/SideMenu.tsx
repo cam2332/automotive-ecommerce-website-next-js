fdaf224c442d4c23b464287217070f39ced0b7d8
cam2332 <kam2332@gmail.com>
2021-07-05 12:35:12 +0200

    Add SideMenu component

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,98 @@
+import tw from 'tailwind-styled-components'
+import { IoCloseSharp } from 'react-icons/io5'
+
+function SideMenu({
+  title,
+  isRight,
+  onClose,
+  visible,
+  children,
+}: {
+  title?: string
+  isRight?: boolean
+  onClose: () => void
+  visible: boolean
+  children?: JSX.Element | JSX.Element[] | undefined
+}) {
+  return (
+    <Wrapper onClick={onClose} $visible={visible}>
+      <Container
+        onClick={(e) => e.stopPropagation()}
+        $visible={visible}
+        $isRight={isRight}>
+        <Header>
+          <CloseIcon onClick={onClose} $isRight={isRight} />
+          {title && <TitleText>{title}</TitleText>}
+        </Header>
+        <Content>{children}</Content>
+      </Container>
+    </Wrapper>
+  )
+}
+
+const Wrapper = tw.div`
+  fixed
+  top-0
+  left-0
+  z-100
+  ${({ $visible }: { $visible: boolean }) =>
+    $visible ? `w-full h-full` : `w-0 h-0`}
+`
+
+const Container = tw.div`
+  fixed
+  top-0
+  w-70%
+  h-full
+  z-100
+  duration-500
+  bg-white
+  shadow-2xl
+  ${({ $visible, $isRight }: { $visible: boolean; $isRight: boolean }) =>
+    $visible
+      ? $isRight
+        ? `right-0`
+        : `left-0`
+      : $isRight
+      ? `-right-full sm:hidden`
+      : `-left-full sm:hidden`}
+
+  sm:w-350px
+  sm:top-14
+  sm:right-2
+  sm:h-auto
+
+  lg:top-20
+  lg:right-50%-512px
+`
+
+const Header = tw.div`
+  flex
+  flex-row-reverse
+  items-center
+  justify-between
+  h-12
+  px-5
+`
+
+const CloseIcon = tw(IoCloseSharp)`
+  text-2xl
+  cursor-pointer
+  text-title-color
+  ${({ $isRight }: { $isRight: boolean }) => !$isRight && 'order-2'}
+`
+
+const TitleText = tw.span`
+  text-lg
+  font-semibold
+  text-primary-color
+`
+
+const Content = tw.div`
+  flex
+  flex-col
+  items-stretch
+  p-5
+`
+
+export default SideMenu
