aee933052391dcf3c6a57b050cf26f85da4f0be3
cam2332 <kam2332@gmail.com>
2021-07-07 16:38:46 +0200

    Add EmptyHeader component

---
+++ src/components/EmptyHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Header from './Header'
+
+function EmptyHeader() {
+  const router = useRouter()
+
+  return (
+    <Header
+      logoText={'Auto części'}
+      onClickLogo={() => router.push('/')}
+      contactPhoneNumber={'01 234 56 78'}
+      contactInfoVisible={true}
+    />
+  )
+}
+
+export default EmptyHeader
