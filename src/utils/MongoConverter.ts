0935986fdc638cbd414935201806544cab90b5cb
cam2332 <kam2332@gmail.com>
2021-08-08 18:02:08 +0200

    Add utility methods for category

---
+++ src/utils/MongoConverter.ts
@@ -0,0 +1,27 @@
+import { ICategory, CategoryDocument } from '../DAO/documents/Category'
+
+export const fromCategoryDocument = (category: CategoryDocument): ICategory => {
+  return {
+    id: category._id || category.id,
+    name: category.name,
+    numberOfProducts: category.numberOfProducts || 0,
+    parentCategoryId: category.parentCategoryId || null,
+    thumbnailUrl: category.thumbnailUrl || null,
+  }
+}
+
+export const createDataTree = (dataset, id: string, parentId: string) => {
+  const hashTable = Object.create(null)
+  dataset.forEach(
+    (aData) => (hashTable[aData[id]] = { ...aData, categories: [] })
+  )
+  const dataTree = []
+  dataset.forEach((aData) => {
+    if (aData[parentId] && hashTable[aData[parentId]]) {
+      hashTable[aData[parentId]].categories.push(hashTable[aData[id]])
+    } else {
+      dataTree.push(hashTable[aData[id]])
+    }
+  })
+  return dataTree
+}
