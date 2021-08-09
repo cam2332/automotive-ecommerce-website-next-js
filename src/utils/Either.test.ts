19662a7fac4f54ff663a81812c81d895ffea1429
cam2332 <kam2332@gmail.com>
2021-08-09 18:58:39 +0200

    Add Either class

---
+++ src/utils/Either.test.ts
@@ -0,0 +1,39 @@
+import { Either, Left, Right, left, right } from './Either'
+
+describe('Either', () => {
+  it('should create instance of Left', () => {
+    const instance = new Left('left')
+    expect(instance.isLeft()).toEqual(true)
+    expect(instance.isRight()).toEqual(false)
+    instance.applyOnLeft((value) => {
+      expect(value).toEqual('left')
+    })
+  })
+
+  it('should create instance of Right', () => {
+    const instance = new Right('right')
+    expect(instance.isLeft()).toEqual(false)
+    expect(instance.isRight()).toEqual(true)
+    instance.applyOnRight((value) => {
+      expect(value).toEqual('right')
+    })
+  })
+
+  it('should create instance of Left using wrapper method', () => {
+    const instance = left('left')
+    expect(instance.isLeft()).toEqual(true)
+    expect(instance.isRight()).toEqual(false)
+    instance.applyOnLeft((value) => {
+      expect(value).toEqual('left')
+    })
+  })
+
+  it('should create instance of Right using wrapper method', () => {
+    const instance = right('right')
+    expect(instance.isLeft()).toEqual(false)
+    expect(instance.isRight()).toEqual(true)
+    instance.applyOnRight((value) => {
+      expect(value).toEqual('right')
+    })
+  })
+})
---
+++ src/utils/Either.ts
@@ -0,0 +1,57 @@
+export type Either<L, R> = Left<L, R> | Right<L, R>
+
+export class Left<L, R> {
+  readonly value: L
+
+  constructor(value: L) {
+    this.value = value
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  applyOnLeft<B>(func: (l: L) => B): Either<B, B> {
+    return new Left(func(this.value))
+  }
+
+  applyOnRight<B>(_: (r: R) => B): Either<L, B> {
+    return this as any
+  }
+}
+
+export class Right<L, R> {
+  readonly value: R
+
+  constructor(value: R) {
+    this.value = value
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  applyOnLeft<B>(_: (l: L) => B): Either<R, B> {
+    return this as any
+  }
+
+  applyOnRight<B>(func: (r: R) => B): Either<L, B> {
+    return new Right(func(this.value))
+  }
+}
+
+export const left = <L, R>(l: L): Either<L, R> => {
+  return new Left<L, R>(l)
+}
+
+export const right = <L, R>(r: R): Either<L, R> => {
+  return new Right<L, R>(r)
+}
