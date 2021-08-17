9ad260725720e382c476e5c86c7f0f7f1ee18d69
cam2332 <kam2332@gmail.com>
2021-08-17 17:42:01 +0200

    Add new method and error handling

--- src/business/CarMakeManager.test.ts
+++ src/business/CarMakeManager.test.ts
@@ -2,6 +2,7 @@ import ApplicationError from '../utils/ApplicationError'
 import * as InMemoryMongo from '../utils/InMemoryMongo'
 import {
   createCarMake,
+  findAllCarMakes,
   findCarMakeById,
   findCarMakeByName,
 } from './CarMakeManager'
@@ -10,136 +11,202 @@ describe('CarMakeManager', () => {
   let carMakeName = 'Audi'
   let carMakeId: string
 
-  beforeAll(async () => await InMemoryMongo.connect())
-  afterAll(async () => await InMemoryMongo.closeDatabase())
-
-  describe('createCarMake', () => {
-    it('should create a car make', async () => {
-      expect.assertions(2)
-      const createdCarMake = await createCarMake({ name: carMakeName })
-
-      createdCarMake.applyOnRight((carMake) => {
-        carMakeId = carMake.id
-        expect(carMake).toBeDefined()
-        expect(carMake.name).toEqual(carMakeName)
-      })
-    })
-
-    it('should not create a car make if name is not defined', async () => {
+  describe('DB not connected', () => {
+    it('should return an error "Internal error" when creating a car make', async () => {
       expect.assertions(1)
-      const createdCarMake = await createCarMake({} as any)
-
-      createdCarMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(
-          ApplicationError.MISSING_REQUIRED_PROPERTY.type
-        )
-      })
-    })
-
-    it('should not create a car make if name is not string', async () => {
-      expect.assertions(1)
-      const createdCarMake = await createCarMake({ name: ['test'] } as any)
-
-      createdCarMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(ApplicationError.UNSUPPORTED_PROPERTY.type)
-      })
-    })
-
-    it('should not create a car make if name is already present in the collection', async () => {
-      expect.assertions(1)
-
-      await createCarMake({ name: carMakeName })
-
       const createdCarMake = await createCarMake({ name: carMakeName })
 
       createdCarMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(ApplicationError.RESOURCE_EXISTS.type)
+        expect(error.type).toEqual(ApplicationError.INTERNAL_ERROR.type)
       })
-    })
-  })
+    }, 10500)
 
-  describe('findCarMakeById', () => {
-    it('should find car make', async () => {
-      expect.assertions(2)
-      const carMake = await findCarMakeById(carMakeId)
-
-      carMake.applyOnRight((carMake) => {
-        expect(carMake.id).toEqual(carMakeId)
-        expect(carMake.name).toEqual(carMakeName)
-      })
-    })
-
-    it('should not find car make if id is not defined', async () => {
+    it('should return an error "Internal error" when searching for car make by id', async () => {
       expect.assertions(1)
-      const carMake = await findCarMakeById(undefined as string)
+      const carMake = await findCarMakeById('testId')
 
       carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(
-          ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.type
-        )
+        expect(error.type).toEqual(ApplicationError.INTERNAL_ERROR.type)
       })
-    })
+    }, 10500)
 
-    it('should not find car make if id is not string', async () => {
+    it('should return an error "Internal error" when searching for car make by name', async () => {
       expect.assertions(1)
-      const carMake = await findCarMakeById({} as string)
-
-      carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(
-          ApplicationError.UNSUPPORTED_PATH_PARAMETER.type
-        )
-      })
-    })
-
-    it('should not find car make if it does not exists', async () => {
-      expect.assertions(1)
-      const carMake = await findCarMakeById('abcdefghijklmno123456789')
-
-      carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(ApplicationError.RESOURCE_NOT_FOUND.type)
-      })
-    })
-  })
-
-  describe('findCarMakeByName', () => {
-    it('should find car make', async () => {
-      expect.assertions(2)
       const carMake = await findCarMakeByName(carMakeName)
 
-      carMake.applyOnRight((carMake) => {
-        expect(carMake.id).toEqual(carMakeId)
-        expect(carMake.name).toEqual(carMakeName)
+      carMake.applyOnLeft((error) => {
+        expect(error.type).toEqual(ApplicationError.INTERNAL_ERROR.type)
+      })
+    }, 10500)
+
+    it('should return an error "Internal error" when searching for a car makes', async () => {
+      expect.assertions(1)
+      const makes = await findAllCarMakes()
+      makes.applyOnLeft((error) => {
+        expect(error.type).toEqual(ApplicationError.INTERNAL_ERROR.type)
+      })
+    }, 10500)
+  })
+
+  describe('DB connected', () => {
+    beforeAll(async () => await InMemoryMongo.connect())
+    afterAll(async () => await InMemoryMongo.closeDatabase())
+
+    describe('createCarMake', () => {
+      it('should create a car make', async () => {
+        expect.assertions(2)
+        const createdCarMake = await createCarMake({ name: carMakeName })
+
+        createdCarMake.applyOnRight((carMake) => {
+          carMakeId = carMake.id
+          expect(carMake).toBeDefined()
+          expect(carMake.name).toEqual(carMakeName)
+        })
+      })
+
+      it('should not create a car make if name is not defined', async () => {
+        expect.assertions(1)
+        const createdCarMake = await createCarMake({} as any)
+
+        createdCarMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(
+            ApplicationError.MISSING_REQUIRED_PROPERTY.type
+          )
+        })
+      })
+
+      it('should not create a car make if name is not string', async () => {
+        expect.assertions(1)
+        const createdCarMake = await createCarMake({ name: ['test'] } as any)
+
+        createdCarMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(ApplicationError.UNSUPPORTED_PROPERTY.type)
+        })
+      })
+
+      it('should not create a car make if name is already present in the collection', async () => {
+        expect.assertions(1)
+
+        await createCarMake({ name: carMakeName })
+
+        const createdCarMake = await createCarMake({ name: carMakeName })
+
+        createdCarMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(ApplicationError.RESOURCE_EXISTS.type)
+        })
       })
     })
 
-    it('should not find car make if name is not defined', async () => {
-      expect.assertions(1)
-      const carMake = await findCarMakeByName(undefined as string)
+    describe('findCarMakeById', () => {
+      it('should find car make', async () => {
+        expect.assertions(2)
+        const carMake = await findCarMakeById(carMakeId)
 
-      carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(
-          ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.type
-        )
+        carMake.applyOnRight((carMake) => {
+          expect(carMake.id).toEqual(carMakeId)
+          expect(carMake.name).toEqual(carMakeName)
+        })
+      })
+
+      it('should not find car make if id is not defined', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeById(undefined as string)
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(
+            ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.type
+          )
+        })
+      })
+
+      it('should not find car make if id is not string', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeById({} as string)
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(
+            ApplicationError.UNSUPPORTED_PATH_PARAMETER.type
+          )
+        })
+      })
+
+      it('should not find car make if it does not exists', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeById('abcdefghijklmno123456789')
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(ApplicationError.RESOURCE_NOT_FOUND.type)
+        })
       })
     })
 
-    it('should not find car make if name is not string', async () => {
-      expect.assertions(1)
-      const carMake = await findCarMakeByName({} as string)
+    describe('findCarMakeByName', () => {
+      it('should find car make', async () => {
+        expect.assertions(2)
+        const carMake = await findCarMakeByName(carMakeName)
 
-      carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(
-          ApplicationError.UNSUPPORTED_PATH_PARAMETER.type
-        )
+        carMake.applyOnRight((carMake) => {
+          expect(carMake.id).toEqual(carMakeId)
+          expect(carMake.name).toEqual(carMakeName)
+        })
+      })
+
+      it('should not find car make if name is not defined', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeByName(undefined as string)
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(
+            ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.type
+          )
+        })
+      })
+
+      it('should not find car make if name is not string', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeByName({} as string)
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(
+            ApplicationError.UNSUPPORTED_PATH_PARAMETER.type
+          )
+        })
+      })
+
+      it('should not find car make if it does not exists', async () => {
+        expect.assertions(1)
+        const carMake = await findCarMakeByName('abcdefghijklmno123456789')
+
+        carMake.applyOnLeft((error) => {
+          expect(error.type).toEqual(ApplicationError.RESOURCE_NOT_FOUND.type)
+        })
       })
     })
 
-    it('should not find car make if it does not exists', async () => {
-      expect.assertions(1)
-      const carMake = await findCarMakeByName('abcdefghijklmno123456789')
+    describe('findAllCarMakes', () => {
+      beforeAll(async () => await InMemoryMongo.clearDatabase())
+      it('should return an empty array', async () => {
+        expect.assertions(1)
+        const makes = await findAllCarMakes()
 
-      carMake.applyOnLeft((error) => {
-        expect(error.type).toEqual(ApplicationError.RESOURCE_NOT_FOUND.type)
+        makes.applyOnRight((carMakes) => {
+          expect(carMakes.length).toEqual(0)
+        })
+      })
+
+      it('should return an array containing one car make', async () => {
+        expect.assertions(2)
+
+        const createdCarMake = await createCarMake({ name: carMakeName })
+
+        if (createdCarMake.isRight) {
+          const makes = await findAllCarMakes()
+
+          makes.applyOnRight((carMakes) => {
+            expect(carMakes).toHaveLength(1)
+            expect(carMakes[0]).toEqual(createdCarMake.value)
+          })
+        }
       })
     })
   })
--- src/business/CarMakeManager.ts
+++ src/business/CarMakeManager.ts
@@ -11,29 +11,52 @@ export const createCarMake = async (requestBody: {
     return left(
       ApplicationError.MISSING_REQUIRED_PROPERTY.setDetail(
         `The 'name' property must be present in the request body.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
   if (typeof requestBody.name !== 'string' || requestBody.name.length < 1) {
     return left(
       ApplicationError.UNSUPPORTED_PROPERTY.setDetail(
         `The 'name' property must be a string.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
 
-  const carMake = await CarMake.findMakeByName(requestBody.name)
+  try {
+    const carMake = await CarMake.findMakeByName(requestBody.name)
 
-  if (carMake) {
+    if (carMake) {
+      return left(
+        ApplicationError.RESOURCE_EXISTS.setDetail(
+          `Car make with name '${requestBody.name}' already exists.`
+        ).setInstance(`/cars/makes/${carMake.id}`)
+      )
+    } else {
+      const createdCarMake = await CarMake.createMake(requestBody.name)
+
+      return right(fromCarMakeDocument(createdCarMake))
+    }
+  } catch (error) {
     return left(
-      ApplicationError.RESOURCE_EXISTS.setDetail(
-        `Car make with name '${requestBody.name}' already exists.`
-      ).setInstance(`/cars/makes/${carMake.id}`)
+      ApplicationError.INTERNAL_ERROR.setDetail(
+        'Cannot create car make.'
+      ).setInstance('/cars/makes')
     )
-  } else {
-    const createdCarMake = await CarMake.createMake(requestBody.name)
+  }
+}
 
-    return right(fromCarMakeDocument(createdCarMake))
+export const findAllCarMakes = async (): Promise<
+  Either<ApplicationError, ICarMake[]>
+> => {
+  try {
+    const makes = await CarMake.findAllMakes()
+    return right(makes.map((carMake) => fromCarMakeDocument(carMake)))
+  } catch (error) {
+    return left(
+      ApplicationError.INTERNAL_ERROR.setDetail(
+        'Cannot find car makes.'
+      ).setInstance('/cars/makes')
+    )
   }
 }
 
@@ -44,26 +67,34 @@ export const findCarMakeById = async (
     return left(
       ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.setDetail(
         `The 'id' URL parameter must be provided in the request.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
   if (typeof id !== 'string' || id.length < 1) {
     return left(
       ApplicationError.UNSUPPORTED_PATH_PARAMETER.setDetail(
         `The 'id' path parameter must be a string.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
 
-  const carMake = await CarMake.findMakeById(id)
+  try {
+    const carMake = await CarMake.findMakeById(id)
 
-  if (carMake) {
-    return right(fromCarMakeDocument(carMake))
-  } else {
-    return left(
-      ApplicationError.RESOURCE_NOT_FOUND.setDetail(
-        `Car make with id ${id} does not exists.`
+    if (carMake) {
+      return right(fromCarMakeDocument(carMake))
+    } else {
+      return left(
+        ApplicationError.RESOURCE_NOT_FOUND.setDetail(
+          `Car make with id ${id} does not exists.`
+        ).setInstance('/cars/makes')
       )
+    }
+  } catch (error) {
+    return left(
+      ApplicationError.INTERNAL_ERROR.setDetail(
+        'Cannot find car make.'
+      ).setInstance('/cars/makes')
     )
   }
 }
@@ -75,26 +106,34 @@ export const findCarMakeByName = async (
     return left(
       ApplicationError.MISSING_REQUIRED_PATH_PARAMETER.setDetail(
         `The 'name' URL parameter must be provided in the request.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
   if (typeof name !== 'string' || name.length < 1) {
     return left(
       ApplicationError.UNSUPPORTED_PATH_PARAMETER.setDetail(
         `The 'name' path parameter must be a string.`
-      )
+      ).setInstance('/cars/makes')
     )
   }
 
-  const carMake = await CarMake.findMakeByName(name)
+  try {
+    const carMake = await CarMake.findMakeByName(name)
 
-  if (carMake) {
-    return right(fromCarMakeDocument(carMake))
-  } else {
-    return left(
-      ApplicationError.RESOURCE_NOT_FOUND.setDetail(
-        `Car make with name ${name} does not exists.`
+    if (carMake) {
+      return right(fromCarMakeDocument(carMake))
+    } else {
+      return left(
+        ApplicationError.RESOURCE_NOT_FOUND.setDetail(
+          `Car make with name ${name} does not exists.`
+        ).setInstance('/cars/makes')
       )
+    }
+  } catch (error) {
+    return left(
+      ApplicationError.INTERNAL_ERROR.setDetail(
+        'Cannot find car make.'
+      ).setInstance('/cars/makes')
     )
   }
 }
