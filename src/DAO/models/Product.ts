00f19989038a2a689a5b3b7358d8e14739f5f141
cam2332 <kam2332@gmail.com>
2021-08-22 16:14:08 +0200

    Add Product model

---
+++ src/DAO/models/Product.ts
@@ -0,0 +1,29 @@
+import { Model, model } from 'mongoose'
+import { ProductDocument } from '../documents/Product'
+import { ResultData } from '../types/ResultData'
+import ProductSchema from '../schemas/ProductSchema'
+import SortMethod from '../types/SortMethod'
+
+export interface ProductModel extends Model<ProductDocument> {
+  createProduct(): Promise<ProductDocument>
+  findProductsByCategoryHierarchy(
+    categoryId: string,
+    userId: string | undefined,
+    page: number,
+    resultsPerPage: number,
+    sortMethod: SortMethod
+  ): Promise<ResultData<ProductDocument[]>>
+}
+
+let product: ProductModel
+try {
+  product = model('Products') as ProductModel
+} catch {
+  product = model<ProductDocument, ProductModel>(
+    'Products',
+    ProductSchema,
+    'Products'
+  )
+}
+
+export default product
