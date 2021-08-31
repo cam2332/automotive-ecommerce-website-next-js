733842f3779000105cc6ff3c0ab8641b2544b5e3
cam2332 <kam2332@gmail.com>
2021-08-31 14:01:47 +0200

    Fix property name

--- src/pages/api/sessions/index.ts
+++ src/pages/api/sessions/index.ts
@@ -2,12 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { loginUser } from '../../../business/UserManager'
 import { IUser } from '../../../DAO/documents/User'
 import { authenticateUser, clearUser } from '../../../services/Tokens'
-import ApplicationError from '../../../utils/ApplicationError'
 import defaultHandler from '../../_defaultHandler'
 
 const handler = defaultHandler<NextApiRequest, NextApiResponse>()
   .post(async (req, res) => {
-    const user = await loginUser(req.body())
+    const user = await loginUser(req.body)
     if (user.isRight()) {
       authenticateUser(res, user.value as IUser)
       res.json(user)
