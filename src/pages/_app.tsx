c2fa81e6620e533b8b4abe41238e172cd5b6b9a7
cam2332 <kam2332@gmail.com>
2021-08-31 13:18:03 +0200

    Update _app with QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import AppProvider from '../context/AppContext'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </QueryClientProvider>
   )
 }
 
