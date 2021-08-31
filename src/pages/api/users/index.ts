468671f5e96ebfa493aa05bbfaf223a11ab28921
cam2332 <kam2332@gmail.com>
2021-08-31 13:07:22 +0200

    Add users api endpoint

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createUser } from '../../../business/UserManager'
+import { IUser } from '../../../DAO/documents/User'
+import { authenticateUser } from '../../../services/Tokens'
+import defaultHandler from '../../_defaultHandler'
+
+const handler = defaultHandler<NextApiRequest, NextApiResponse>().post(
+  async (req, res) => {
+    const user = await createUser(req.body)
+
+    if (user.isRight()) {
+      authenticateUser(res, user.value as IUser)
+      res.json(user)
+    } else {
+      res.status(user.value.status).json(user.value.toObject())
+    }
+  }
+)
+
+export default handler
