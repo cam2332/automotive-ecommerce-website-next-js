1bc9783bf5d5c0582df54c404fb91f39e2ca564f
cam2332 <kam2332@gmail.com>
2021-09-01 12:28:39 +0200

    Fix and add get endpoint to verify token

--- src/pages/api/sessions/index.ts
+++ src/pages/api/sessions/index.ts
@@ -1,17 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { loginUser } from '../../../business/UserManager'
+import { findUserById, loginUser } from '../../../business/UserManager'
 import { IUser } from '../../../DAO/documents/User'
-import { authenticateUser, clearUser } from '../../../services/Tokens'
+import {
+  authenticateUser,
+  clearUser,
+  verifyToken,
+} from '../../../services/Tokens'
+import ApplicationError from '../../../utils/ApplicationError'
 import defaultHandler from '../../_defaultHandler'
 
 const handler = defaultHandler<NextApiRequest, NextApiResponse>()
   .post(async (req, res) => {
     const user = await loginUser(req.body)
     if (user.isRight()) {
-      authenticateUser(res, user.value as IUser)
-      res.json(user)
+      const token = authenticateUser(res, user.value as IUser)
+      res.json({ user: user.value, token: token })
     } else {
       res.status(user.value.status).json(user.value.toObject())
+    }
+  })
+  .get(async (req, res) => {
+    const token = req.headers.authorization
+    if (!token) {
+      const error =
+        ApplicationError.UNAUTHORIZED.setDetail(
+          'No token provided.'
+        ).setInstance('/sessions')
+      res.status(error.status).json(error.toObject())
+    }
+
+    try {
+      const data = verifyToken(token)
+      const user = await findUserById(data.id)
+      if (user.isRight()) {
+        const newToken = authenticateUser(res, user.value as IUser)
+        res.json({ user: user.value, token: newToken })
+      } else {
+        res.status(user.value.status).json(user.value.toObject())
+      }
+    } catch (err) {
+      const error = ApplicationError.UNAUTHORIZED.setDetail(
+        'Error verifying token.'
+      ).setInstance('/sessions')
+      res.status(error.status).json(error.toObject())
     }
   })
   .delete((_req, res) => {
