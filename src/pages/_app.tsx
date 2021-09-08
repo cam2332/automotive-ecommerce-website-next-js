5f70a373275910ace71048991bfbe0f0319e0df3
cam2332 <kam2332@gmail.com>
2021-09-08 17:54:27 +0200

    Uninstall react-query package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
 import AppProvider from '../context/AppContext'
-
-const queryClient = new QueryClient()
 import SessionProvider from '../context/SessionContext'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
     <SessionProvider>
       <AppProvider>
         <Component {...pageProps} />
       </AppProvider>
-    </QueryClientProvider>
     </SessionProvider>
   )
 }
