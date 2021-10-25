58b299140f6e0407f293004e29f6b888e988fe51
cam2332 <kam2332@gmail.com>
2021-10-25 12:54:24 +0200

    Add api endpoint to get list of products by ids

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import dbConnect from '../../../utils/dbConnect'
+import { findProductsByIds } from '../../../business/ProductManager'
+import SortMethod from '../../../DAO/types/SortMethod'
+import ApplicationError from '../../../utils/ApplicationError'
+
+//TODO: get user from DB using auth cookie from request
+export default async function handler(
+  { method, query: { ids, page, resultsPerPage, sortMethod } }: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await dbConnect()
+  } catch (err) {
+    const error = ApplicationError.INTERNAL_ERROR.setDetail(
+      'The server is currently unable to complete the request.'
+    ).setInstance('/products')
+
+    res.status(error.status).json(error.toObject())
+  }
+
+  switch (method) {
+    case 'GET':
+      try {
+        const results = await findProductsByIds(
+          Array.isArray(ids)
+            ? ids.filter((id) => id.length > 0)
+            : ids.split(',').filter((id) => id.length > 0),
+          undefined,
+          parseInt(page as string) || 1,
+          parseInt(resultsPerPage as string) || 999,
+          SortMethod.fromType(sortMethod as string) || SortMethod.relevance
+        )
+        results.applyOnLeft((error) =>
+          res.status(error.status).json(error.toObject())
+        )
+        results.applyOnRight((products) => {
+          res.status(200).json(products)
+        })
+      } catch (err) {
+        const error = ApplicationError.INTERNAL_ERROR.setDetail(
+          'Cannot find products.'
+        ).setInstance('/products')
+
+        res.status(error.status).json(error.toObject())
+      }
+      break
+    default:
+      const error = ApplicationError.METHOD_NOT_ALLOWED.setDetail(
+        `The '${method}' method is not supported.`
+      ).setInstance('/products')
+
+      res.status(error.status).json(error.toObject())
+      break
+  }
+}
