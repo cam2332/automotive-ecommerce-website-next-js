e1ba7620ad29a6b5dff32d82ac045ada38e5c34a
cam2332 <kam2332@gmail.com>
2021-11-02 11:17:49 +0100

    Add SessionManager

---
+++ src/business/SessionManager.ts
@@ -0,0 +1,37 @@
+import { IUser } from '../DAO/documents/User'
+import { authenticateUser, verifyToken } from '../services/Tokens'
+import ApplicationError from '../utils/ApplicationError'
+import { Either, left, right } from '../utils/Either'
+import { findUserById } from './UserManager'
+
+const WRONG_TOKEN = ApplicationError.UNAUTHORIZED.setDetail(
+  'Error verifying token.'
+).setInstance('/sessions')
+
+const NO_TOKEN =
+  ApplicationError.UNAUTHORIZED.setDetail('No token provided.').setInstance(
+    '/sessions'
+  )
+
+export const authorize = async (
+  req,
+  res
+): Promise<Either<ApplicationError, { user: IUser; token: string }>> => {
+  const token = req.headers.authorization || req.cookies.auth
+  if (!token || token.length === 0) {
+    return left(NO_TOKEN)
+  }
+
+  try {
+    const data = verifyToken(token)
+    const user = await findUserById(data.id)
+    if (user.isRight()) {
+      const newToken = authenticateUser(res, user.value as IUser)
+      return right({ user: user.value, token: newToken })
+    } else {
+      return left(WRONG_TOKEN)
+    }
+  } catch (err) {
+    return left(WRONG_TOKEN)
+  }
+}
