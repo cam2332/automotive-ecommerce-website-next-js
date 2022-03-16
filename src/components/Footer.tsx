be0cccc8e4ec6924a7ad5fb751cfd4f5541a761e
cam2332 <kam2332@gmail.com>
2022-03-16 16:39:24 +0100

    Add link to contact page in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
 /* eslint-disable jsx-a11y/anchor-is-valid */
 function Footer() {
   return (
     <footer>
       <div className='flex flex-col items-center justify-center w-full h-24 p-4 space-y-4 text-center bg-gray-100'>
         <div className='flex flex-row items-center space-x-3'>
-          <a href='#' className='text-sm'>
-            Kontakt
-          </a>
+          <Link key='contact' href='/contact'>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className='text-sm'>Kontakt</a>
+          </Link>
           <div className='w-px h-3 bg-primary-color' />
           <a href='#' className='text-sm'>
             Płatność
