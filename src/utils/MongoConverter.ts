e789a49a0c22ceb24d1b45e856c8fae50943fdbf
cam2332 <kam2332@gmail.com>
2022-03-27 12:43:46 +0200

    Update method creating tree of categories

--- src/utils/MongoConverter.ts
+++ src/utils/MongoConverter.ts
@@ -4,6 +4,7 @@ import { CarTypeDocument, ICarType } from '../DAO/documents/CarType'
 import { ICategory, CategoryDocument } from '../DAO/documents/Category'
 import { IProduct, ProductDocument } from '../DAO/documents/Product'
 import { IUser, UserDocument } from '../DAO/documents/User'
+import ICategoryCriteria from '../DAO/types/ICategoryCriteria'
 
 export const fromCarTypeDocument = (carType: CarTypeDocument): ICarType => ({
   id: carType._id || carType.id,
@@ -48,18 +49,22 @@ export const fromCategoryDocument = (
   thumbnailUrl: category.thumbnailUrl || null,
 })
 
-export const createDataTree = (dataset, id: string, parentId: string) => {
-  const hashTable = Object.create(null)
-  dataset.forEach(
-    (aData) =>
-      (hashTable[aData[id]] = {
-        ...aData,
-        categories: [],
-        numberOfProducts: aData.numberOfProducts || 0,
-      })
-  )
+export const createCategoryDataTree = (
+  dataset: ICategory[],
+  id: string,
+  parentId: string,
+  criteria: ICategoryCriteria
+) => {
+  const hashTable: { [key: string]: ICategory } = Object.create(null)
+  dataset.forEach((aData: ICategory) => {
+    hashTable[aData[id]] = {
+      ...aData,
+      categories: [],
+      numberOfProducts: aData.numberOfProducts || 0,
+    }
+  })
   const dataTree: ICategory[] = []
-  dataset.forEach((aData) => {
+  dataset.forEach((aData: ICategory) => {
     if (aData[parentId] && hashTable[aData[parentId]]) {
       hashTable[aData[parentId]].categories.push(hashTable[aData[id]])
       hashTable[aData[parentId]].numberOfProducts +=
@@ -69,14 +74,46 @@ export const createDataTree = (dataset, id: string, parentId: string) => {
     }
   })
 
-  dataTree.forEach((aData) => {
+  dataTree.forEach((aData: ICategory) => {
     aData.numberOfProducts = aData.categories.reduce(
       (accumulator, currentValue) =>
         accumulator + currentValue.numberOfProducts,
       0
     )
+    if (aData.categories) {
+      aData.categories.sort((subCategory1, subCategory2) => {
+        if (criteria.sort.order === 'DESC') {
+          return (
+            subCategory1[criteria.sort.attribute] -
+            subCategory2[criteria.sort.attribute]
+          )
+        }
+        return (
+          subCategory2[criteria.sort.attribute] -
+          subCategory1[criteria.sort.attribute]
+        )
+      })
+    }
   })
   return dataTree
+    .filter((category) => new RegExp(criteria.name, 'i').test(category.name))
+    .slice(
+      (criteria.pagination.page - 1) * criteria.pagination.size,
+      (criteria.pagination.page - 1) * criteria.pagination.size +
+        criteria.pagination.size
+    )
+    .sort((subCategory1, subCategory2) => {
+      if (criteria.sort.order === 'DESC') {
+        return (
+          subCategory1[criteria.sort.attribute] -
+          subCategory2[criteria.sort.attribute]
+        )
+      }
+      return (
+        subCategory2[criteria.sort.attribute] -
+        subCategory1[criteria.sort.attribute]
+      )
+    })
 }
 
 export const fromProductDocument = (product: ProductDocument): IProduct => ({
