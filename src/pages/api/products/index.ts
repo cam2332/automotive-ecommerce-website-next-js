21f45b01372e63534ca314ab1ef50c17d2429ba1
cam2332 <kam2332@gmail.com>
2022-04-03 15:34:45 +0200

    Update api/products endpoint

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,18 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '../../../utils/dbConnect'
-import {
-  findProductsByIds,
-  findAllByTitle,
-} from '../../../business/ProductManager'
-import SortMethod from '../../../DAO/types/SortMethod'
+import { findAll } from '../../../business/ProductManager'
 import ApplicationError from '../../../utils/ApplicationError'
+import ProductCriteriaBuilder from '../../../DAO/types/ProductCriteriaBuilder'
+import PageableBuilder from '../../../DAO/types/PageableBuilder'
+import SortCriteriaBuilder from '../../../DAO/types/SortCriteriaBuilder'
 
 // TODO: get user from DB using auth cookie from request
 export default async function handler(
-  {
-    method,
-    query: { title, ids, page, resultsPerPage, sortMethod },
-  }: NextApiRequest,
+  { method, query }: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
@@ -28,32 +24,38 @@ export default async function handler(
   switch (method) {
     case 'GET':
       try {
-        if (title) {
-          const results = await findAllByTitle(
-            title as string,
-            parseInt(page as string, 10) || 1,
-            parseInt(resultsPerPage as string, 10) || 999,
-            SortMethod.fromType(sortMethod as string) || SortMethod.relevance
-          )
-
-          res.status(200).json(results)
-        } else {
-          const results = await findProductsByIds(
-            Array.isArray(ids)
-              ? ids.filter((id) => id.length > 0)
-              : ids.split(',').filter((id) => id.length > 0),
-            undefined,
-            parseInt(page as string) || 1,
-            parseInt(resultsPerPage as string) || 999,
-            SortMethod.fromType(sortMethod as string) || SortMethod.relevance
-          )
-          results.applyOnLeft((error) =>
-            res.status(error.status).json(error.toObject())
-          )
-          results.applyOnRight((products) => {
-            res.status(200).json(products)
-          })
-        }
+        const products = await findAll(
+          new ProductCriteriaBuilder()
+            .withTitle(query.title as string)
+            .withIds(
+              query.ids &&
+                (Array.isArray(query.ids)
+                  ? query.ids.filter((id) => id.length > 0)
+                  : query.ids.split(',').filter((id) => id.length > 0))
+            )
+            .withCategoriesIds(
+              query.categoriesIds &&
+                (Array.isArray(query.categoriesIds)
+                  ? query.categoriesIds.filter((id) => id.length > 0)
+                  : query.categoriesIds
+                      .split(',')
+                      .filter((id) => id.length > 0))
+            )
+            .withPagination(
+              new PageableBuilder()
+                .withPage(parseInt(query.page as string, 10) || 1)
+                .withSize(parseInt(query.size as string, 10) || 10)
+                .build()
+            )
+            .withSort(
+              new SortCriteriaBuilder()
+                .withOrder(query['sort.order'] as string)
+                .withAttribute((query['sort.attribute'] as string) || 'name')
+                .build()
+            )
+            .build()
+        )
+        res.status(200).json(products)
       } catch (err) {
         const error = ApplicationError.INTERNAL_ERROR.setDetail(
           'Cannot find products.'
